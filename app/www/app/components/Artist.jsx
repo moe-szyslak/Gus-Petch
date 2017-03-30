ae7e424753de6b60f0cecc99704fb94d2eecaec8
Moe Szyslak <moe.duffdude@gmail.com>
2017-03-30 14:06:07 +0300

    default _artist image_
    - a circle will do [to save space]
    - close #17

--- app/www/app/components/Artist.jsx
+++ app/www/app/components/Artist.jsx
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react';
 function Artist({ artist }) {
   return (
     <div className="artist">
-      <div className="artist__image" style={{ background: `transparent url('${artist.image}') 50% 50% / cover no-repeat` }} />
+      <div className="artist__image" style={{ background: `transparent url('${artist.image}') 50% 50% / cover no-repeat`, border: artist.image ? 'none' : '1px solid rgba(29, 161, 242, .5)' }} />
       <span className="artist__name">{ artist.name }</span>
     </div>
   );
